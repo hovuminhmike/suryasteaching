df0f6845ec03148a9e3e6bfe1636cc676e368a0a
Minh Ho <hovuminhmike@gmail.com>
2018-11-09 10:21:25 -0800

    weeeeeeeeee
    description

---
+++ FrcProject_04/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,186 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Robot extends IterativeRobot 
+{	
+	double rightJoyStickx, rightJoySticky;
+	Joysticks stick; 
+	//This function is run whenever the robot starts. This function is used for any initialization of code
+	@Override
+	public void robotInit() 
+	{
+		stick = new Joysticks();
+	}
+
+	 //This function runs once, right before autonomous period starts. 
+	@Override
+	public void autonomousInit() 
+	{
+
+	}
+
+	//This is the function that is called during the autonomous period
+	//This function runs periodically, meaning it acts as an infinite loop
+	@Override
+	public void autonomousPeriodic() 
+	{
+
+	}
+
+	//This is the function that is called during the Tele-operated period
+	//This function runs periodically, meaning it acts as an infinite loop
+	@Override
+	public void teleopPeriodic() 
+	{  
+		stick.updateMainController();
+		double angle = getDegrees();
+		double mag = getMagnitude();
+		if(angle == 0)
+		{
+			Motors.rightMotor.set(mag);
+			Motors.leftMotor.set(mag);
+		}
+		else if(angle == 90)
+		{
+			Motors.rightMotor.set(-mag);
+			Motors.leftMotor.set(mag);
+		}
+		else if(angle == 180)
+		{
+			Motors.rightMotor.set(-mag);
+			Motors.leftMotor.set(mag);
+		}
+		else if(angle == 270)
+		{
+			Motors.rightMotor.set(mag);
+			Motors.leftMotor.set(-mag);
+		}
+		else if(angle == 45)
+		{
+			Motors.rightMotor.set(0);
+			Motors.leftMotor.set(mag);
+		}
+		else if(angle == 135)
+		{
+			Motors.rightMotor.set(-mag);
+			Motors.leftMotor.set(0);
+		}
+		else if(angle == 225)
+		{
+			Motors.rightMotor.set(mag);
+			Motors.leftMotor.set(0);
+		}
+		else if(angle == 315)
+		{
+			Motors.rightMotor.set(0);
+			Motors.leftMotor.set(-mag);
+		}
+		else if(angle > 0 && angle < 45)
+		{
+			Motors.rightMotor.set(mag * Math.cos(Math.toRadians(angle)));
+			Motors.leftMotor.set(mag * Math.sin(Math.toRadians(angle)));
+		}
+		else if(angle > 45 && angle < 90)
+		{
+			Motors.rightMotor.set(-mag * Math.cos(Math.toRadians(angle)));
+			Motors.leftMotor.set(-mag * Math.sin(Math.toRadians(angle)));
+		}
+		// else if(angle > 90 && angle < 135)
+		// {
+			
+		// }
+		// else if(angle > 135 && angle < 180)
+		// {
+
+		// }
+		// else if(angle > 180 && angle < 225)
+		// {
+
+		// }
+		// else if(angle > 225 && angle < 270)
+		// {
+
+		// }
+		// else if(angle > 270 && angle < 315)
+		// {
+
+		// }
+		// else if(angle > 315 && angle < 359)
+		// {
+
+		// }
+		else 
+		{
+			Motors.rightMotor.set(0);
+			Motors.leftMotor.set(0);
+		}
+	}
+
+	//This is the function that is called during the test
+	//Test is an option available in the driver station and can be used to test specific pieces of code.
+	//This function runs periodically, meaning it acts like an infinite loop
+	@Override
+	public void testPeriodic() 
+	{
+		
+	}
+	
+	//This function returns the angle of the vector of the right Joystick values
+	//You may need to edit this function depending upon what type of controller you are using
+	public static double getDegrees()
+	{
+		if(Joysticks.rightJoySticky > 0  && Joysticks.rightJoyStickx > 0) //1st quadrant
+		{
+			return ( 270 +Math.toDegrees(Math.atan(Joysticks.rightJoyStickx/Joysticks.rightJoySticky)));
+		}
+		if(Joysticks.rightJoySticky > 0  && Joysticks.rightJoyStickx < 0)
+		{
+			return 180- Math.toDegrees(Math.atan(Joysticks.rightJoySticky/Joysticks.rightJoyStickx));
+		}
+		if(Joysticks.rightJoySticky < 0  && Joysticks.rightJoyStickx < 0)
+		{
+			return 180- Math.toDegrees(Math.atan(Joysticks.rightJoySticky/Joysticks.rightJoyStickx));
+		}
+		if(Joysticks.rightJoySticky < 0  && Joysticks.rightJoyStickx > 0)
+		{
+			return -Math.toDegrees(Math.atan(Joysticks.rightJoySticky/Joysticks.rightJoyStickx));
+		}
+		if(Joysticks.rightJoySticky == 0  && Joysticks.rightJoyStickx < 0)
+		{
+			return 180;
+		}
+		if(Joysticks.rightJoySticky == 0  && Joysticks.rightJoyStickx < 0)
+		{
+			return 0;
+		}
+		if(Joysticks.rightJoySticky > 0  && Joysticks.rightJoyStickx == 0)
+		{
+			return 270;
+		}
+		if(Joysticks.rightJoySticky < 0  && Joysticks.rightJoyStickx == 0)
+		{
+			return 90;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+	//This function returns the magnitude of the vector of the right Joystick
+	public static double getMagnitude()
+	{
+		return Math.sqrt(Math.pow(Joysticks.rightJoySticky, 2) + Math.pow(Joysticks.rightJoyStickx, 2));
+	}
+	
+	public void testFunctions()
+	{
+		double magnitude, angle;
+		magnitude =  getMagnitude();
+		angle = getDegrees();
+		
+		System.out.println("Magnitude: " + magnitude + "; " + "Angle: " + angle + ";");
+	}
+}
